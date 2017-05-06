f2c50d15bc6f19e0f28700114c989e4e48e306f5
Bruno Gama Catao <brunogamacatao@gmail.com>
2017-05-06 14:57:51 -0300

    adicionado o express para implementar o servidor, com mongoose

--- server.js
+++ server.js
@@ -16,3 +16,28 @@ new WebpackDevServer(webpack(config), {
 
   console.log('Listening on http://localhost:3000');
 });
+
+// Iniciando o servidor rest
+const URL_BANCO_DE_DADOS = 'mongodb://localhost/aula11';
+const PORTA_DO_SERVIDOR = 9000;
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Abre uma conexão com o banco de dados
+mongoose.connect(URL_BANCO_DE_DADOS);
+mongoose.connection.once('open', function() {
+  // Espera abrir a conexão com o banco de dados
+
+  // Cria uma aplicação express
+  var app = express();
+
+  app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+
+  app.listen(PORTA_DO_SERVIDOR, function() {
+    console.log(`Servidor Rest ouvindo na porta ${PORTA_DO_SERVIDOR}`);
+    console.log(`URL base Rest http://localhost:${PORTA_DO_SERVIDOR}`);
+  });
+});
